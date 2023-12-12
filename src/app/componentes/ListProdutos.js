8e7e5037c7d02018e5785b60494c81a81912f5ca
Giovanniscastro <89524918+Giovanniscastro@users.noreply.github.com>
2023-12-12 20:06:49 -0300

    ListProdutos ok

--- src/app/componentes/ListProdutos.js
+++ src/app/componentes/ListProdutos.js
@@ -1,18 +1,29 @@
 'use client'
-import CardProduto from "./CardProduto"
 import axios from "axios";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import CardProduto from "./CardProduto";
 
 export default function ListProdutos() {
-
+    const searchParams = useSearchParams();
     const [listaProdutos, setListaProdutos] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:1337/api/produtos/?populate=*")
             .then(function(response) { 
-              setListaProdutos(response.data.data);
+                const category = searchParams.get('categoryId')
+                const produtos = response.data.data
+
+                if(category) {
+                    const filtered = produtos.filter(produto => {
+                        return produto.attributes.category.data && produto.attributes.category.data.id === parseInt(category)
+                    })
+                    return setListaProdutos(filtered);
+                }
+
+                return setListaProdutos(produtos);
             })
-    }, [])
+    }, [searchParams.get('categoryId')])
 
     return (
         listaProdutos.map((prod) => 
