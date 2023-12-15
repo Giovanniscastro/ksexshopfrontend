e2204ed8dac136a05f6fb88a93def3163e43fea1
Giovanniscastro <89524918+Giovanniscastro@users.noreply.github.com>
2023-12-15 18:22:55 -0300

    Update CardProduto.js

--- src/app/componentes/CardProduto.js
+++ src/app/componentes/CardProduto.js
@@ -17,7 +17,7 @@ export default function CardProduto({ produto }) {
     <div className="w3-col l4 m6 s12 w3-container w3-padding-16">
       <div className="w3-card">
         <div className="w3-container w3-center">
-          <img src={url} style={{ width: "20%" }} />
+          <img src={url} />
           <h5>{produto.attributes.Name}</h5>
           <h3 className="w3-blue">
             {produto.attributes.Preco.toLocaleString("pt-BR", {
@@ -30,9 +30,16 @@ export default function CardProduto({ produto }) {
             <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
                 <h2>{produto.attributes.Name}</h2>
                 <img id="imgmodal" src={url} style={{ width: "20%" }} />
-                <h2>R${produto.attributes.Preco}</h2>
+                <h2>{produto.attributes.Preco.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}</h2>
                 <h3>Descrição</h3>
                 <p>{produto.attributes.Descricao}</p>
+                <h5>Entre em contato</h5>
+                <a href="https://wa.me/5197319458?text=Gostaria+de+comprar+esse+produto+estou+te+mandando+o+print+dele" class="whatsapp-button" target="_blank">
+                  WhatsApp
+                </a>
             </Modal>
         </div>
       </div>
